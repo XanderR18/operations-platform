193313d508cc67b283557ee5b1ff1e33755b445f
XanderR18 <rodriguezxander18@gmail.com>
2026-07-16 18:55:19 -0600

    Add verification to machine service CRUD

--- app/models/machine.py
+++ app/models/machine.py
@@ -1,6 +1,6 @@
 class Machine():
-    def __init__(self, host_name, IP, MAC):
-        self.id = 0
+    def __init__(self, id, host_name, IP, MAC):
+        self.id = id
         self.host_name = host_name
         self.ip = IP
         self.mac = MAC
--- app/services/machine_service.py
+++ app/services/machine_service.py
@@ -14,21 +14,40 @@ class MachineService():
         return
     
     def add_machine(self, machine:Machine):
-        self.machines[machine.id] = machine
+        machine_id = machine.id
+        if machine_id in self.machines:
+            machine_id += "(1)"
+
+        self.machines[machine_id] = machine
 
     def remove_machine(self, machine_id):
+        if machine_id not in self.machines:
+            raise MachineNotFound(f"Machine with id: {machine_id} not found.")
+        
         del self.machines[machine_id]
 
     def update_machine(self, machine:Machine):
+        if machine.id not in self.machines:
+            raise MachineNotFound(f"Machine with id: {machine.id} not found.")
+        
         self.machines[machine.id] = machine
 
     def get_machine(self, machine_id):
+        if machine_id not in self.machines:
+            raise MachineNotFound(f"Machine with id: {machine_id} not found.")
+
         return self.machines[machine_id]
 
     def get_all_machines(self):
+        if not self.machines:
+            return 0
+
         return self.machines
     
-    # Hard coding for testing. Not permanent
-    Machine("compute-main", "192.168.0.20", "70-54-D2-AB-1B-5D")
-    Machine("network-main", "192.168.0.30", "94-C6-91-A2-A4-61")
-    Machine("storage-main", "192.168.0.40", "B4-2E-99-3B-4A-B3")
+class MachineNotFound(Exception):
+    pass
+
+# Hard coding for testing. Not permanent
+Machine("cm1", "compute-main", "192.168.0.20", "70-54-D2-AB-1B-5D")
+Machine("nm1", "network-main", "192.168.0.30", "94-C6-91-A2-A4-61")
+Machine("sm1", "storage-main", "192.168.0.40", "B4-2E-99-3B-4A-B3")
